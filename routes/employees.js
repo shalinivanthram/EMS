f6c66005c5274eefdb6e6d14f0f59c9da84ad806
bipneswar <bmangara@in.ibm.com>
2017-02-07 17:38:03 +0530

    Design changes in search

--- routes/employees.js
+++ routes/employees.js
@@ -7,7 +7,7 @@ var emp = require('../controller/employee');
 router.post('/login', function(req, res, next) {
     require('passport').authenticate('local', {failureRedirect: '/?error=error.login_error'})(req, res, next);
 }, function(req, res, next) {
-    res.render("listEmployees");
+    res.redirect("/employees/listEmployees");
 });
 
 router.get('/logout', function(req, res, next) {
